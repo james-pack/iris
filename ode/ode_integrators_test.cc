1d0f1514fd7822562bf8697feb87b1d19b254247
James Pack <jgrimm@gmail.com>
2021-11-28 15:35:38 -0500

    Moved tests for all ODE integrators into one test file

--- ode/ode_integrators_test.cc
+++ ode/ode_integrators_test.cc
@@ -1,10 +1,35 @@
-#include "ode/backward_euler.h"
-
 #include <functional>
 
 #include "gtest/gtest.h"
+#include "ode/backward_euler.h"
+#include "ode/forward_euler.h"
 
 namespace pack::ode {
+
+TEST(ForwardEulerTest, IntegratorCorrectOnFirstStep) {
+  int deriv_call_count{0};
+  std::function<double(double)> deriv = [&deriv_call_count](double) -> double {
+    ++deriv_call_count;
+    return 1.;
+  };
+  double time_step{1.};
+  auto integrator = ForwardEuler(0., 0., time_step, deriv);
+  EXPECT_EQ(1., integrator());
+  EXPECT_EQ(1, deriv_call_count);
+}
+
+TEST(ForwardEulerTest, ReturnedIntegratorCanBeCalledMultipleTimes) {
+  int deriv_call_count{0};
+  std::function<double(double)> deriv = [&deriv_call_count](double) -> double {
+    ++deriv_call_count;
+    return 1.;
+  };
+  double time_step{1.};
+  auto integrator = ForwardEuler(0., 0., time_step, deriv);
+  EXPECT_EQ(time_step, integrator());
+  EXPECT_EQ(2. * time_step, integrator());
+  EXPECT_EQ(2, deriv_call_count);
+}
 
 TEST(BackwardEulerTest, IntegratorCorrectOnFirstStep) {
   int deriv_call_count{0};
